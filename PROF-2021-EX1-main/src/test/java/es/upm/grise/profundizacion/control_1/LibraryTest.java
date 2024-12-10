74f1318d0b0021cf095fed23e5feb576e204bea1
irenemartnez <117649832+irenemartnez@users.noreply.github.com>
2024-12-10 22:09:03 +0100

    Se añaden libros correctamente (uno o varios; comprobad la condición de libro repetido) Se eliminan libros correctamente (comprobar la condición de lista vacía y libro no existente). Se localizan libros correctamente (comprobar la condición de lista vacía y libro no existente).

--- PROF-2021-EX1-main/src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
+++ PROF-2021-EX1-main/src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
@@ -1,10 +1,62 @@
 package es.upm.grise.profundizacion.control_1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class LibraryTest {
+	Library library;
+	Book book1;
+	Book book2;
 	
-	@Test
-	public void test() {
-		fail();
+	@BeforeEach
+	void setUp() {
+		library = new Library();
+		book1 = new Book("Title1");
+		book2 = new Book("Title2");
 	}
+	@Test
+	void testAddBook() throws DuplicatedBookException, NonExistingBookException, EmptyLibraryException {
+		library.addBook(book1);
+		assertEquals(book1, library.getBook("Title1"));
+	}
+	@Test 
+	void testAddBook2() throws DuplicatedBookException, NonExistingBookException, EmptyLibraryException {
+		library.addBook(book1);
+		assertThrows(DuplicatedBookException.class, () -> library.addBook(book1));
+	}
+	@Test
+	void testRemoveBook() throws DuplicatedBookException, NonExistingBookException, EmptyLibraryException {
+		library.addBook(book1);
+		library.addBook(book2);
+		library.removeBook(book1);
+		assertThrows(NonExistingBookException.class, () -> library.getBook("Title1"));
+	}
+	@Test
+	void testRemoveBook2() throws DuplicatedBookException, NonExistingBookException, EmptyLibraryException {
+		library.addBook(book1);
+		library.removeBook(book1);
+		assertThrows(EmptyLibraryException.class, () -> library.getBook("Title1"));
+	}
+
+	@Test
+	void testGetBook() throws DuplicatedBookException, NonExistingBookException, EmptyLibraryException {
+		library.addBook(book1);
+		assertEquals(book1, library.getBook("Title1"));
+	}
+	@Test
+	void testGetBook2() throws DuplicatedBookException, NonExistingBookException, EmptyLibraryException {
+		library.addBook(book2);
+		assertThrows(NonExistingBookException.class, () -> library.getBook("Title1"));
+	}
+
+
 
 }
